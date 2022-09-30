70f8c8d53023530e5404bf5979dee0d4e2ad56ad
Marina Rikkonen <marina.rikkonen@yandex.ru>
2022-09-30 19:09:07 +0300

    change test_main_page "test_go_to_login_page"

--- test_main_page.py
+++ test_main_page.py
@@ -15,7 +15,7 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_should_be_login_page(browser):
+def test_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
