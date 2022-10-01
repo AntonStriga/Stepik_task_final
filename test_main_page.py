1a70d29668797359210103b430ff1f11ae3b8d57
Marina Rikkonen <marina.rikkonen@yandex.ru>
2022-10-01 15:01:56 +0300

    in file "test_main_page" initialize the Login Page in the body of the test "test_guest_can_go_to_login_page"

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,5 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,6 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page_login = LoginPage(browser, browser.current_url)
+    page_login.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -15,13 +16,7 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    page_login = LoginPage(browser, link)
-    page_login.should_be_login_page()
+
 
 
 
