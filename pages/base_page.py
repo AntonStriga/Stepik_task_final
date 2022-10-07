b4707f9e4a68fd33721a8001b769ffa388c1fb68
Marina Rikkonen <marina.rikkonen@yandex.ru>
2022-10-07 23:16:04 +0300

    in file add "go_to_basket"

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,7 @@ import math
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from .locators import BasePageLocators
+from .locators import BasePageLocators, MainPageLocators
 
 class BasePage():
 
@@ -59,4 +59,8 @@ class BasePage():
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_basket(self):
+        button = self.browser.find_element(*MainPageLocators.BUTTON_BASKET)
+        button.click()
 
